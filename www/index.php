b7e6f8285d19c04c0c4559b6606205ad63cbe0ac
mihej <mihej@rambler.ru>
2021-04-18 23:37:28 +0300

    создание, подключение и проверка первых контроллеров и экшенов

--- www/index.php
+++ www/index.php
@@ -1,2 +1,13 @@
 <?php
-    phpinfo();
+    // Проверяем существование в массиве _GET параметра controller
+    $controllerName = isset($_GET['controller']) ? ucfirst($_GET['controller']) : 'index';
+    echo '$controllerName = ' . $controllerName . '<br>';
+
+    // Проверяем в _GET параметр action
+    $actionName = isset($_GET['action']) ? $_GET['action'] : 'index';
+    echo '$actionName = ' . $actionName . '<br>';
+
+    // Подключаем контроллер
+    include_once '../controllers/' . $controllerName . 'Controller.php';
+
+    testAction();
