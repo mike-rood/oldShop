42a5bef0f1a87d7a99c52ab4a40580ec3695a8d3
mihej <mihej@rambler.ru>
2021-10-10 16:17:27 +0300

    Функции добавления товара в корзину покупок

--- controllers/CartController.php
+++ controllers/CartController.php
@@ -6,5 +6,18 @@ include_once '../models/ProductsModel.php';
 
 //Добавление продукта в корзину
 function addtocartAction() {
+    $itemId = isset($_GET['id']) ? intval($_GET['id']) : null;
+    if ( ! $itemId) return false;
 
+    $resData = array();
+
+    //Если значение не найдено, то добавляем
+    if (isset($_SESSION['cart']) && array_search($itemId, $_SESSION['cart']) === false) {
+        $_SESSION['cart_id'][] = $itemId;
+        $resData['cntItems'] = count($_SESSION['cart']);
+        $resData['success'] = 1;
+    } else {
+        $resData['success'] = 0;
+    }
+    echo json_encode($resData);
 }
--- www/js/main.js
+++ www/js/main.js
@@ -1,3 +1,17 @@
 function addToCart(itemId) {
-    
+    console.log("js - addToCart()");
+    $.ajax({
+        type: 'POST',
+        async: false,
+        url: "/cart/addtocart/" + itemId + '/',
+        dataType: 'json',
+        success: function(data) {
+            if (data['success']) {
+                $('#cartCntItems').html(data['cntItems']);
+
+                $('#addCart_'+ itemId).hide();
+                $('#removeCart_'+ itemId).show();
+            }
+        }
+    })
 }
