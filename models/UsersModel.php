8a03662604d7a9558ba1019d0bc400b40c52ff9d
mihej <mihej@rambler.ru>
2021-10-18 13:31:37 +0300

    login пользователя
    	modified:   controllers/UserController.php
    	modified:   models/UsersModel.php

--- models/UsersModel.php
+++ models/UsersModel.php
@@ -45,3 +45,29 @@ function checkUserEmail($email) {
 
     return $rs;
 }
+
+/**
+ * Авторизация пользователя
+ * 
+ * @param string $email почта (логин)
+ * @param string $pwd пароль
+ * @return array массив данных пользователя 
+ */
+function loginUser($email, $pwd)
+{
+    $email = htmlspecialchars(mysql_real_escape_string($email));
+    $pwd = md5($pwd);
+
+    $sql = "SELECT * FROM `users`
+            WHERE (`email` = '{$email}' and `pwd` = '{$pwd}')
+            LIMIT 1";
+    
+    $rs = mysql_query($sql);
+    $rs = createSmartyRsArray($rs);
+    if (isset($rs[0])) {
+        $rs['success'] = 1;
+    } else {
+        $rs['success'] = 0;
+    }
+    return $rs;
+}
